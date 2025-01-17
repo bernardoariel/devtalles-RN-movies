efc46aeed9762ed4d3b4e8166d95502d0ea5a9fc
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2025-01-16 22:27:39 -0300

    Agregando colores

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,27 +1,47 @@
-import { View, Text, ActivityIndicator, ScrollView,TouchableOpacity  } from 'react-native'
-import React from 'react'
-import { useMovies } from '@/presentation/hooks/useMovies'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import MainSlideshow from '@/presentation/components/movies/MainSlideshow'
-import MovieHorizontalList from '@/presentation/components/movies/MovieHorizontalList'
+import { View, StatusBar, StyleSheet } from 'react-native';
+import React from 'react';
 import { useRouter } from 'expo-router';
-import SearchProduct from '@/presentation/components/products/SearchProduct'
+import SearchProduct from '@/presentation/components/products/SearchProduct';
+
 const HomeScreen = () => {
   const router = useRouter();
 
-  const productos = [
-    { id: 1, name: 'Producto 1' },
-    { id: 2, name: 'Producto 2' },
-    { id: 3, name: 'Producto 3' },
-  ]; // Datos de ejemplo
-
   return (
-    <ScrollView>
-      <View style={{ padding: 16 }}>
-       <SearchProduct />
+    <>
+      {/* Cambia el estilo de la barra de estado */}
+      <StatusBar backgroundColor="#FCB800" barStyle="light-content" />
+
+      <View style={styles.container}>
+        {/* Contenedor fijo con bordes redondeados */}
+        <View style={styles.header}>
+          <SearchProduct />
+        </View>
+        {/* Otros contenidos */}
+        <View style={styles.content}>
+          {/* Coloca aquí el contenido que deseas */}
+        </View>
       </View>
-    </ScrollView>
+    </>
   );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  header: {
+    backgroundColor: '#FCB800',
+    padding: 16,
+    borderBottomLeftRadius: 24,
+    borderBottomRightRadius: 24,
+    zIndex: 1, // Asegura que SearchProduct esté por encima
+  },
+  content: {
+    flex: 1,
+    marginTop: -16, // Ajusta la posición si el desplegable cubre demasiado
+    backgroundColor: '#252525',
+  },
+});
--- presentation/components/products/SearchProduct.tsx
+++ presentation/components/products/SearchProduct.tsx
@@ -1,9 +1,11 @@
 import { View, Text, TextInput, StyleSheet, Button, ScrollView, TouchableOpacity, Keyboard, Dimensions } from 'react-native';
-import React, { useEffect, useRef, useState } from 'react';
-import { useFocusEffect, useRouter } from 'expo-router';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'expo-router';
 import { useProducts } from '@/presentation/hooks/useProducts';
 import { ProductsResponse } from '@/infrastructure/interfaces/productos.interface';
 import { useMarcas } from '@/presentation/hooks/useMarcas';
+import { Ionicons } from '@expo/vector-icons';
+
 
 const SearchProduct = () => {
   const [searchTerm, setSearchTerm] = useState(''); // Término de búsqueda
@@ -11,20 +13,10 @@ const SearchProduct = () => {
   const [keyboardHeight, setKeyboardHeight] = useState(0);
   const screenHeight = Dimensions.get('window').height;
   const router = useRouter();
-  const inputRef = useRef<TextInput>(null);
+
   // Hook para productos
   const { productos, isLoading } = useProducts();
-  const { marcas } = useMarcas()
-
-  useFocusEffect(
-    React.useCallback(() => {
-      setSearchTerm(''); // Limpia el término de búsqueda
-      inputRef.current?.focus(); // Coloca el foco en el campo de entrada
-    }, [])
-  );
-  useEffect(() => {
-    inputRef.current?.focus(); // Coloca el foco en el campo de entrada
-  }, []);
+  const { marcas } = useMarcas();
 
   useEffect(() => {
     const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', (e) => {
@@ -40,32 +32,24 @@ const SearchProduct = () => {
       keyboardDidHideListener.remove();
     };
   }, []);
+
   useEffect(() => {
-    console.log('Término de búsqueda:', searchTerm);
-  
     if (searchTerm.trim() && isNaN(Number(searchTerm))) {
       const term = searchTerm.toLowerCase();
-      console.log('Término transformado:', term);
-  
+
       // Filtrar productos
       const filteredProductos = productos.filter((producto) =>
         producto.Producto?.toLowerCase().includes(term)
       );
-  
-      console.log('Productos filtrados:', filteredProductos);
-  
+
       // Filtrar marcas
-      const filteredMarcas = marcas!.filter((marca) =>
+      const filteredMarcas = marcas?.filter((marca) =>
         marca.Marca?.toLowerCase().includes(term)
       );
-  
-      console.log('Marcas filtradas:', filteredMarcas);
-  
+
       // Combinar resultados de productos y marcas
-      const combinedResults = [...filteredProductos, ...filteredMarcas];
-  
-      console.log('Resultados combinados:', combinedResults);
-  
+      const combinedResults = [...filteredProductos, ...(filteredMarcas || [])];
+
       // Actualizar el estado solo si los resultados son diferentes
       if (
         combinedResults.length !== filteredResults.length ||
@@ -77,69 +61,80 @@ const SearchProduct = () => {
       setFilteredResults([]);
     }
   }, [searchTerm, productos, marcas]);
-  
 
   const handleSearch = () => {
     const trimmedTerm = searchTerm.trim();
 
-    
-  if (trimmedTerm && !isNaN(Number(trimmedTerm))) {
-    // Si es un número, navegar al producto por ID
-    router.push({
-      pathname: `/product/[id]`,
-      params: { id: trimmedTerm },
-    });
-  } else if (trimmedTerm) {
-    // Si es texto, navegar a la lista de productos filtrados
-    router.push({
-      pathname: `/product/products`,
-      params: { searchTerm: trimmedTerm },
-    });
-  }
+    if (!trimmedTerm) {
+      console.log("El término de búsqueda está vacío.");
+      return; // No hacer nada si el término está vacío
+    }
+
+    if (!isNaN(Number(trimmedTerm))) {
+      // Si es un número, navegar al producto por ID
+      router.push({
+        pathname: `/product/[id]`,
+        params: { id: trimmedTerm },
+      });
+    } else {
+      // Si es texto, navegar a una lista de productos filtrados
+      router.push({
+        pathname: `/product/products`,
+        params: { searchTerm: trimmedTerm },
+      });
+    }
   };
 
   const handleResultClick = (result: ProductsResponse) => {
-    console.log('Producto seleccionado:', result);
     router.push({
       pathname: `/product/[id]`,
       params: { id: result.CodProducto },
     });
   };
+
   const extraMargin = 20; // Descuento adicional
-const dropdownHeight = screenHeight - keyboardHeight - 150 - extraMargin;
+  const dropdownHeight = screenHeight - keyboardHeight - 150 - extraMargin;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Buscar Producto</Text>
+      <Text style={styles.title}>Buscar...</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nombre del Producto o ID"
-        onChangeText={(text) => setSearchTerm(text)}
-        value={searchTerm}
-      />
-
-      <Button title="Buscar Producto" onPress={handleSearch} />
+      <View style={styles.searchWrapper}>
+        <View style={styles.searchContainer}>
+          <Ionicons name="search" size={20} color="#999" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Buscar ..."
+            placeholderTextColor="#999"
+            onChangeText={(text) => setSearchTerm(text)}
+            value={searchTerm}
+          />
+        </View>
+        <TouchableOpacity style={styles.roundButton} onPress={handleSearch}>
+          <Ionicons name="play" size={20} color="orange" />
+        </TouchableOpacity>
+      </View>
 
       {isLoading ? (
         <Text style={styles.loading}>Cargando productos...</Text>
       ) : filteredResults.length > 0 ? (
         <ScrollView
-  style={[styles.dropdown, { maxHeight: dropdownHeight }]}
-  keyboardShouldPersistTaps="handled"
-  nestedScrollEnabled={true}
->
-  {filteredResults.map((item, index) => (
-    <TouchableOpacity key={index} onPress={() => handleResultClick(item)}>
-      <Text style={styles.listItem}>
-        {'CodProducto' in item
-          ? `${item.Producto} - ${item.CodProducto}`
-          : `${item.Marca}`}
-      </Text>
-    </TouchableOpacity>
-  ))}
-</ScrollView>
+          style={[styles.dropdown, { maxHeight: dropdownHeight }]}
+          keyboardShouldPersistTaps="handled"
+          nestedScrollEnabled={true}
+        >
+          {filteredResults.map((item, index) => (
+            <TouchableOpacity key={index} onPress={() => handleResultClick(item)}>
+              <Text style={styles.listItem}>
+                {'CodProducto' in item
+                  ? `${item.Producto} - ${item.CodProducto}`
+                  : `${item.Marca}`}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
       ) : (
-        <Text style={styles.noResults}>No hay resultados</Text>
+        <Text style={styles.noResults}></Text>
       )}
     </View>
   );
@@ -148,35 +143,73 @@ const dropdownHeight = screenHeight - keyboardHeight - 150 - extraMargin;
 const styles = StyleSheet.create({
   container: {
     padding: 16,
+    position: 'relative', // Importante para la lista desplegable
+    zIndex: 1, // Asegura que el dropdown se muestre encima
   },
   title: {
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
+    color: '#444',
+  },
+  searchWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  searchContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#FCB800',
+    borderRadius: 25,
+    backgroundColor: '#FFE5B4',
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  icon: {
+    marginRight: 10,
+    color: '#FCB800',
   },
   input: {
+    flex: 1,
+    height: 40,
+    color: '#6B4226',
+  },
+  roundButton: {
+    width: 45,
+    height: 45,
+    borderRadius: 10,
+    backgroundColor: '#FFE5B4', // Fondo blanco
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
     borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
+    borderColor: '#FCB800', // Borde naranja fuerte para mantener consistencia
     marginBottom: 10,
-    borderRadius: 5,
   },
   dropdown: {
-    marginTop: 10,
+    position: 'absolute', // Superpone el dropdown
+    top: 100, // Ajusta la posición desde la parte superior del input
+    left: 0,
+    right: 0,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: 'orange',
     borderRadius: 5,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF8E7',
+    maxHeight: 250, // Altura máxima de la lista
+    zIndex: 2, // Prioridad sobre otros elementos
   },
   listItem: {
     padding: 10,
     borderBottomWidth: 1,
-    borderBottomColor: '#eee',
+    borderBottomColor: '#FCB800',
+    color: '#444',
   },
   loading: {
     marginTop: 10,
     textAlign: 'center',
-    color: '#666',
+    color: '#FCB800',
   },
   noResults: {
     marginTop: 10,
