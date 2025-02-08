7a4b2f3f7c8de92b56dd5346d78a2050b25aff09
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2025-02-08 11:47:47 -0300

    Agregando stock

--- presentation/components/products/ProductDetails.tsx
+++ presentation/components/products/ProductDetails.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import colors from '@/config/helpers/colors';
 
 const ProductDetails = ({ producto, findMarcasById }) => {
   return (
     <View style={styles.container}>
+      <View style={styles.stockBadge}>
+          <Text style={styles.stockBadgeText}>{producto.Stock > 0 ? `${producto.Stock} Unidades` : 'Sin stock'}</Text>
+        </View>
       <Text style={styles.title}>{producto.Producto}</Text>
       <Text style={styles.description}>{producto.Descripcion}</Text>
       <View style={styles.row}>
@@ -43,6 +46,23 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: colors.primary.light,
   },
+  stockBadge: {
+    position: 'absolute',
+    alignSelf: 'center',
+    top: -25,// Posicionado en la mitad entre la imagen y el siguiente div
+    backgroundColor: colors.primary.main,
+    borderRadius: 12,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    zIndex: 3,
+    elevation: 5,
+  },
+  stockBadgeText: {
+    color: colors.neutral.dark,
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+
 });
 
 export default ProductDetails;
